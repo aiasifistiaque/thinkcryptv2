056c986d128f6af218a609e03d50afc4c3dae5bc
Asif Istiaque <asifistiaque.ai@gmail.com>
2022-06-21 21:48:38 +0600

    ss

--- components/portfolio/Portfoliopage.js
+++ components/portfolio/Portfoliopage.js
@@ -8,11 +8,30 @@ import Link from 'next/link';
 
 const data = [
 	{
+		src: '/portfolio/checkliiist',
+		name: 'Checkliiist Inventory Management system',
+		type: 'Inventory Management Saas',
+		href: 'https://checkliiist.com',
+	},
+	{
+		src: '/portfolio/home/6',
+		name: 'Arewa Books',
+		type: 'Web App',
+		href: 'https://arewabooks.com',
+	},
+	{
 		src: '/portfolio/home/1',
 		name: 'SAMC Bangladesh',
 		type: 'Web App',
 		href: 'https://samcbangladesh.org',
 	},
+	{
+		src: '/portfolio/home/ui',
+		name: 'UI/UX projects',
+		type: 'UI/UX',
+		href: 'https://dribbble.com/thinkcrypt',
+	},
+
 	{
 		src: '/portfolio/home/2',
 		name: 'Weather App',
@@ -25,6 +44,16 @@ const data = [
 	// 	type: 'iOs & Android App',
 	// 	href:''
 	// },
+
+	{
+		src: '/portfolio/home/3',
+		name: 'ArewaBooks App',
+		type: 'iOs & Android App',
+		href: 'https://play.google.com/store/apps/details?id=com.arewa.arewabooksapp&hl=en_US&gl=US',
+	},
+];
+
+const data2 = [
 	{
 		src: '/portfolio/home/4',
 		name: `Vincent's Sphere`,
@@ -32,37 +61,10 @@ const data = [
 		href: 'https://vincentsphere.com',
 	},
 	{
-		src: '/portfolio/home/3',
-		name: 'ArewaBooks App',
-		type: 'iOs & Android App',
-		href: 'https://play.google.com/store/apps/details?id=com.arewa.arewabooksapp&hl=en_US&gl=US',
-	},
-	{
 		src: '/portfolio/home/8',
 		name: 'Grocery Shop',
 		type: 'Ecommerce webapp',
 		href: 'https://gshop-psi.vercel.app/',
-	},
-];
-
-const data2 = [
-	{
-		src: '/portfolio/home/5',
-		name: 'Legal Template',
-		type: 'Web App',
-		href: 'https://template-legal.vercel.app/',
-	},
-	{
-		src: '/portfolio/home/ui',
-		name: 'UI/UX projects',
-		type: 'UI/UX',
-		href: 'https://dribbble.com/thinkcrypt',
-	},
-	{
-		src: '/portfolio/home/6',
-		name: 'Arewa Books',
-		type: 'Web App',
-		href: 'https://arewabooks.com',
 	},
 	{
 		src: '/portfolio/home/7',
@@ -70,11 +72,18 @@ const data2 = [
 		type: 'Web App',
 		href: 'https://dignityinternationalagency.com',
 	},
+
 	{
 		src: '/portfolio/home/ciphertext',
 		name: 'Cipher Text',
 		type: 'iOs & Android App',
 		href: 'https://github.com/aiasifistiaque/chatmama',
+	},
+	{
+		src: '/portfolio/home/5',
+		name: 'Legal Template',
+		type: 'Web App',
+		href: 'https://template-legal.vercel.app/',
 	},
 ];
 
